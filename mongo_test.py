9a0e6ab380fb913e23da8e1e933c58d3cc948baa
Durid Ahmad <ahmad.durid96@gmail.com>
2021-01-12 10:19:17 -0700

    created connection to the database

--- mongo_test.py
+++ mongo_test.py
@@ -1,7 +1,13 @@
 from pymongo import MongoClient
 from pprint import pprint
+from secretsImport import Secrets
 
-user_name = ""
-password = ""
-dbname = ""
+user_name = Secrets["mongo_user"]
+password = Secrets["mongo_pass"]
+dbname = "durid-tutorial-db"
+
 url = f'mongodb+srv://{user_name}:{password}@cluster0.1uunq.mongodb.net/{dbname}?retryWrites=true&w=majority'
+print(url)
+
+client = MongoClient(url)
+print()
