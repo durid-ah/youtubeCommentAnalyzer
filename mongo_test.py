9953e9530b1b2b0502fa3cfdd0b36bed6c7009cc
Durid Ahmad <ahmad.durid96@gmail.com>
2021-01-12 08:37:49 -0700

    connection string stub

--- mongo_test.py
+++ mongo_test.py
@@ -1,3 +1,7 @@
 from pymongo import MongoClient
 from pprint import pprint
 
+user_name = ""
+password = ""
+dbname = ""
+url = f'mongodb+srv://{user_name}:{password}@cluster0.1uunq.mongodb.net/{dbname}?retryWrites=true&w=majority'
